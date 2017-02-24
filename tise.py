9eede9f5f883e1924393fff8b78f9d006531d71d
Remy Wang <remywang@protonmail.com>
2017-02-24 17:45:44 -0500

    3d

--- tise.py
+++ tise.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.integrate import odeint
 import scipy.optimize as opt
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as mpl
+from mpl_toolkits.mplot3d import Axes3D
 
 E0 = 1.5 # constant energy
 E1 = 10.5
@@ -25,31 +26,31 @@ psi_l = np.zeros(iterations)
 for i in range(iterations):
     sol = odeint(tise, y0, x, args=(E[i],))
     # plot psi over psi'
-    plt.plot(x, sol[:, 0], 'b', label='$\Psi$')
-    #plt.plot(x, sol[:, 1], 'g', label="$\dot{\Psi}$")
+    mpl.plot(x, sol[:, 0], 'b', label='$\Psi$')
+    #mpl.plot(x, sol[:, 1], 'g', label="$\dot{\Psi}$")
     psi_l[i] = sol[-1,0]
 """
 
 
 """
-#plt.legend(loc='best')
-plt.xlabel('x')
-plt.ylabel('psi(x)')
+#mpl.legend(loc='best')
+mpl.xlabel('x')
+mpl.ylabel('psi(x)')
 
-#plt.show()
+#mpl.show()
 
-plt.clf()
-plt.plot(E,psi_l)
-plt.show()
+mpl.clf()
+mpl.plot(E,psi_l)
+mpl.show()
 """
 
 
 def findSignChange(f):
-	changes = [];
-	for i in range(0, len(f)-1):
-		if(f[i+1]*f[i]<=0):
-			changes.append(i)
-	return changes
+    changes = [];
+    for i in range(0, len(f)-1):
+        if(f[i+1]*f[i]<=0):
+            changes.append(i)
+    return changes
 
 def getFcn(l):
     def fcn(E):
@@ -63,37 +64,46 @@ def getFcn(l):
 lengths = np.linspace(5,30,101)
 print('sign changes:')
 guesses = findSignChange(psi_l)
-print(E[guesses])	
-plt.clf()
+print(E[guesses])
+mpl.clf()
 rootConvergence = []
 
-for j in range(100,101):
-	
-	f = getFcn(lengths[j])
-	
-	psi_l = np.zeros(len(E))
-	for k in range(0,len(E)):
-		psi_l[k] = f(E[k])
-	guesses = findSignChange(psi_l)
-	
-	roots = []
-	for k in range(0,len(guesses)):
-		root = opt.newton(f, E[guesses[k]])
-		plt.scatter(k,root)
-		#roots.append(root)
-	#rootConvergence.append(roots)
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = mpl.figure()
+ax = fig.gca(projection='3d')
 
 
-#plt.plot(rootConvergence.slice())
-plt.show()
-		
+for j in range(0,101):
+
+    f = getFcn(lengths[j])
+
+    psi_l = np.zeros(len(E))
+    for k in range(0,len(E)):
+        psi_l[k] = f(E[k])
+    guesses = findSignChange(psi_l)
+
+    roots = []
+    for k in range(0,len(guesses)):
+        root = opt.newton(f, E[guesses[k]])
+        # mpl.scatter(k,root)
+        ax.scatter(k, root, lengths[j], label='parametric curve')
+        roots.append(root)
+    #rootConvergence.append(roots)
+
+# ax.plot_surface(guesses, roots, lengths, label='parametric curve')
+
+#mpl.plot(rootConvergence.slice())
+mpl.show()
+
 """
 num_l = 1
 roots = np.zeros(num_l)
 for i in range(num_l):
     roots[i] = bisect(getFcn(l*i/num_l), E0,E1)
-    
-plt.clf()
-plt.plot(np.linspace(0,l,num_l), roots)
-plt.show()
+
+mpl.clf()
+mpl.plot(np.linspace(0,l,num_l), roots)
+mpl.show()
 """
