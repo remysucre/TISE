f68a1f470921d27ad2d8e5e90ef45e155fa56c18
Zachary Faber Manning <zfabermanning@gmail.com>
2017-02-25 17:10:09 -0500

    cleaning

--- tise.py
+++ tise.py
@@ -4,47 +4,19 @@ import scipy.optimize as opt
 import matplotlib.pyplot as mpl
 from mpl_toolkits.mplot3d import Axes3D
 
-E0 = 1.5 # constant energy
-E1 = 10.5
-iterations = 20
 
-E = np.linspace(E0,E1,iterations)
+
 y0 = [0.0, 1.0] # inital psi and psi'
-l = 10
-x = np.linspace(0, l, 101) # position
 
 # time-independent schroedinger's equation with semi-infinite V
+# given y = [psi, psi'] returns [psi', psi'']
 def tise(y, x, E):
     psi, u = y # u is d/dx*psi
     dydx = [u, 2*(x*psi - E*psi)]
     return dydx
 
-# numerical solution of TISE
-psi_l = np.zeros(iterations)
 
-"""
-for i in range(iterations):
-    sol = odeint(tise, y0, x, args=(E[i],))
-    # plot psi over psi'
-    mpl.plot(x, sol[:, 0], 'b', label='$\Psi$')
-    #mpl.plot(x, sol[:, 1], 'g', label="$\dot{\Psi}$")
-    psi_l[i] = sol[-1,0]
-"""
-
-
-"""
-#mpl.legend(loc='best')
-mpl.xlabel('x')
-mpl.ylabel('psi(x)')
-
-#mpl.show()
-
-mpl.clf()
-mpl.plot(E,psi_l)
-mpl.show()
-"""
-
-
+# takes array of psi(l), returns all indices just before psi(l) crosses zero
 def findSignChange(f):
     changes = [];
     for i in range(0, len(f)-1):
@@ -52,54 +24,37 @@ def findSignChange(f):
             changes.append(i)
     return changes
 
-def getFcn(l):
-    def fcn(E):
+#get function psi(E) with the max length l
+def getPsiFcn(l):
+	#returns psi at length l
+    def psi_l(E):
         x = np.linspace(0,l,101)
         psi = odeint(tise, y0, x, args=(E,))
         return psi[-1,0]
-    return fcn
+    return psi_l
 
+def findEigenVals(lengths, Es, getPsiFcn):
+	rootConvergence = []
+	for length in lengths:
 
-#for each length, i
-lengths = np.linspace(5,30,101)
-print('sign changes:')
-guesses = findSignChange(psi_l)
-print(E[guesses])
-mpl.clf()
-rootConvergence = []
+		psiFcn = getPsiFcn(length)
 
+		#sample values of psi_l(E)
+		psi_l = []
+		for E in Es:
+		    psi_l.append(psiFcn(E))
+		
+		guesses = findSignChange(psi_l)
 
-mpl.rcParams['legend.fontsize'] = 10
-
-fig = mpl.figure()
-ax = fig.gca(projection='3d')
-
-"""
-for j in range(70,101):
-
-    f = getFcn(lengths[j])
-
-    psi_l = np.zeros(len(E))
-    for k in range(0,len(E)):
-        psi_l[k] = f(E[k])
-    guesses = findSignChange(psi_l)
-
-    roots = []
-    for k in range(0,len(guesses)):
-        #root = opt.newton(f, E[guesses[k]])
-       	upper = E[guesses[k]+1]
-       	lower = E[guesses[k]]
-       	root = opt.bisect(f,lower, upper)
-        #mpl.scatter(k,root)
-        #ax.scatter(k, root, lengths[j], label='parametric curve')
-        roots.append(root)
-    #rootConvergence.append(roots)
-
-# ax.plot_surface(guesses, roots, lengths, label='parametric curve')
-
-#mpl.plot(rootConvergence.slice())
-mpl.show()
-"""
+		roots = []
+		for guess in guesses:
+		    #root = opt.newton(f, E[guesses[k]])
+		   	upper = E[guess+1]
+		   	lower = E[guess]
+		   	root = opt.bisect(psiFcn ,lower, upper)
+		   	roots.append(root)
+		rootConvergence.append(roots)
+	return rootConvergence
 
 def normalize(f,dx):
 	integral= 0
@@ -109,18 +64,33 @@ def normalize(f,dx):
 	integral = integral*(dx)
 	return integral
 
+
+numE = 20
+numL = 100
+
+E0 = 1.5
+E1 = 10.5
+L0 = 5
+L1 = 30
+
+Es = np.linspace(E0,E1,numE)
+lengths = np.linspace(L0,L1,numL)
+
+findEigenVals(lengths,Es,getPsiFcn)
+
+
+"""
+mpl.rcParams['legend.fontsize'] = 10
+fig = mpl.figure()
+ax = fig.gca(projection='3d')
+ax.plot_surface(guesses, roots, lengths, label='parametric curve')
+mpl.plot(rootConvergence.slice())
+mpl.show()
+"""
+
 s = odeint(tise, y0, x, args=(1.85575704051,))
 
 norm = normalize(s[:,0]	,l/100)
 mpl.plot(x,s[:,0]*norm)
 mpl.show()
-"""
-num_l = 1
-roots = np.zeros(num_l)
-for i in range(num_l):
-    roots[i] = bisect(getFcn(l*i/num_l), E0,E1)
 
-mpl.clf()
-mpl.plot(np.linspace(0,l,num_l), roots)
-mpl.show()
-"""
