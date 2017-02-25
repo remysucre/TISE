f6507e97c08b2ccc311d1bb45138f83346b7a3d5
Zachary Faber Manning <zfabermanning@gmail.com>
2017-02-25 17:43:48 -0500

    scatter plotting eigenvals

--- tise.py
+++ tise.py
@@ -11,27 +11,27 @@ y0 = [0.0, 1.0] # inital psi and psi'
 # time-independent schroedinger's equation with semi-infinite V
 # given y = [psi, psi'] returns [psi', psi'']
 def tise(y, x, E):
-    psi, u = y # u is d/dx*psi
-    dydx = [u, 2*(x*psi - E*psi)]
-    return dydx
+	psi, u = y # u is d/dx*psi
+	dydx = [u, 2*(x*psi - E*psi)]
+	return dydx
 
 
 # takes array of psi(l), returns all indices just before psi(l) crosses zero
 def findSignChange(f):
-    changes = [];
-    for i in range(0, len(f)-1):
-        if(f[i+1]*f[i]<=0):
-            changes.append(i)
-    return changes
+	changes = [];
+	for i in range(0, len(f)-1):
+		if(f[i+1]*f[i]<=0):
+			changes.append(i)
+	return changes
 
 #get function psi(E) with the max length l
 def getPsiFcn(l):
 	#returns psi at length l
-    def psi_l(E):
-        x = np.linspace(0,l,101)
-        psi = odeint(tise, y0, x, args=(E,))
-        return psi[-1,0]
-    return psi_l
+	def psi_l(E):
+		x = np.linspace(0,l,101)
+		psi = odeint(tise, y0, x, args=(E,))
+		return psi[-1,0]
+	return psi_l
 
 def findEigenVals(lengths, Es, getPsiFcn):
 	rootConvergence = []
@@ -42,43 +42,44 @@ def findEigenVals(lengths, Es, getPsiFcn):
 		#sample values of psi_l(E)
 		psi_l = []
 		for E in Es:
-		    psi_l.append(psiFcn(E))
-		
+			psi_l.append(psiFcn(E))
 		guesses = findSignChange(psi_l)
-
 		roots = []
 		for guess in guesses:
 		    #root = opt.newton(f, E[guesses[k]])
-		   	upper = E[guess+1]
-		   	lower = E[guess]
-		   	root = opt.bisect(psiFcn ,lower, upper)
-		   	roots.append(root)
+			upper = Es[guess+1]
+			lower = Es[guess]
+			root = opt.bisect(psiFcn ,lower, upper)
+			roots.append(root)
 		rootConvergence.append(roots)
 	return rootConvergence
 
 def normalize(f,dx):
 	integral= 0
 	for i in f:
-		integral += i
+		integral += i**2
 		
 	integral = integral*(dx)
 	return integral
 
 
 numE = 20
-numL = 100
+numL = 50
 
 E0 = 1.5
 E1 = 10.5
-L0 = 5
+L0 = .1
 L1 = 30
 
 Es = np.linspace(E0,E1,numE)
 lengths = np.linspace(L0,L1,numL)
 
-findEigenVals(lengths,Es,getPsiFcn)
-
-
+eigenMat = findEigenVals(lengths,Es,getPsiFcn)
+mpl.clf()
+for i in range(0,len(lengths)):
+	for r in eigenMat[i]:
+		mpl.scatter(lengths[i], r)
+mpl.show()
 """
 mpl.rcParams['legend.fontsize'] = 10
 fig = mpl.figure()
@@ -87,10 +88,12 @@ ax.plot_surface(guesses, roots, lengths, label='parametric curve')
 mpl.plot(rootConvergence.slice())
 mpl.show()
 """
-
+"""
+x = np.linspace(L0,L1,numL)
 s = odeint(tise, y0, x, args=(1.85575704051,))
 
-norm = normalize(s[:,0]	,l/100)
+norm = normalize(s[:,0]	,(L1-L0)/numL)
+mpl.clf()
 mpl.plot(x,s[:,0]*norm)
 mpl.show()
-
+"""
